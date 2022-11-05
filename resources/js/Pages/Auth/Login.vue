20899df5b46106a418bf66fad7cfeab7cba2ac81
Gabeta Soro <gabeta.soro@gmail.com>
2022-11-05 13:04:30 +0000

    wip

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,12 +1,7 @@
 <script setup>
 import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
-import AuthenticationCard from '@/Components/AuthenticationCard.vue';
-import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
-import Checkbox from '@/Components/Checkbox.vue';
+import AuthLayout from '@/Layouts/AuthLayout.vue';
 import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
 
 defineProps({
     canResetPassword: Boolean,
@@ -30,60 +25,61 @@ const submit = () => {
 </script>
 
 <template>
-    <Head title="Log in" />
+    <auth-layout title="Ravi de vous revoir.">
+        <div>
+            <jet-validation-errors class="mb-4" />
 
-    <AuthenticationCard>
-        <template #logo>
-            <AuthenticationCardLogo />
-        </template>
-
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+            <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                {{ status }}
+            </div>
         </div>
-
         <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                />
-                <InputError class="mt-2" :message="form.errors.email" />
+            <div class="space-y-4">
+                <div>
+                    <label class="block text-sm font-medium mb-1" for="email">Adresse mail</label>
+                    <input id="email" class="form-input w-full"
+                           type="email" v-model="form.email" required autofocus />
+                    <InputError class="mt-2" :message="form.errors.email" />
+                </div>
+                <div>
+                    <label class="block text-sm font-medium mb-1" for="password">Mot de passe</label>
+                    <input id="password" class="form-input w-full"
+                           type="password" v-model="form.password" required autocomplete="current-password"  />
+                </div>
             </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-                <TextInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="current-password"
-                />
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
+            <div class="mt-4 mr-1">
                 <label class="flex items-center">
-                    <Checkbox v-model:checked="form.remember" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                    <input type="checkbox" class="form-checkbox">
+                    <span class="text-sm ml-2">Se souvenir de moi</span>
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
+            <div class="flex items-center justify-between mt-6">
+                <div class="mr-1">
+                    <Link v-if="canResetPassword" :href="route('password.request')"
+                          class="text-sm underline hover:no-underline">Mot de passe oublié ?
+                    </Link>
+                </div>
+                <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-3">
+                    Se connecter
+                </button>
             </div>
         </form>
-    </AuthenticationCard>
+        <!-- Footer -->
+        <div class="pt-5 mt-6 border-t border-gray-200">
+            <div class="text-sm">
+                Vous n'avez pas de compte ?
+            </div>
+            <!-- Warning -->
+            <div class="mt-5">
+                <div class="bg-yellow-100 text-yellow-600 px-3 py-2 rounded">
+                    <svg class="inline w-3 h-3 shrink-0 fill-current" viewBox="0 0 12 12">
+                        <path d="M10.28 1.28L3.989 7.575 1.695 5.28A1 1 0 00.28 6.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 1.28z" />
+                    </svg>
+                    <span class="text-sm">
+                        Veuillez contacter les administrateurs de votre IEP.
+                    </span>
+                </div>
+            </div>
+        </div>
+    </auth-layout>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -21,21 +21,12 @@ use Inertia\Inertia;
 Route::domain('{tenant}.'.config('app.domain'))
     ->middleware('tenant')
     ->group(function() {
-        Route::get('/', function () {
-            return Inertia::render('Welcome', [
-                'canLogin' => Route::has('login'),
-                'canRegister' => Route::has('register'),
-                'laravelVersion' => Application::VERSION,
-                'phpVersion' => PHP_VERSION,
-            ]);
-        });
-
         Route::middleware([
             'auth:sanctum',
             config('jetstream.auth_session'),
             'verified',
         ])->group(function () {
-            Route::get('/dashboard', function () {
+            Route::get('/', function () {
                 return Inertia::render('Dashboard');
             })->name('dashboard');
         });
